0a36340763a5cf2c2c6c67484de743b71ee28f2a
Jon Anderson <jon.david.anderson@gmail.com>
2019-10-19 17:47:14 -0700

    Added concurrent, randomized test.

--- mailbox/mailbox_test.go
+++ mailbox/mailbox_test.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"sort"
+	"sync"
 	"testing"
 	"time"
 )
@@ -21,9 +22,10 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestPutAndGet(t *testing.T) {
+// Test basic operation in `Array` mode.
+func TestPutThenGet(t *testing.T) {
 	const mailBoxSize = 5
-	const message = "m"
+	const message = "msg"
 	const priority = 0
 
 	box := NewMailBox(mailBoxSize)
@@ -39,9 +41,10 @@ func TestPutAndGet(t *testing.T) {
 	}
 }
 
-func TestGetAndPut(t *testing.T) {
+// Test basic operation in `Array` mode.
+func TestGetThenPut(t *testing.T) {
 	const mailBoxSize = 5
-	const message = "m"
+	const message = "msg"
 	const priority = 0
 
 	box := NewMailBox(mailBoxSize)
@@ -61,12 +64,14 @@ func TestGetAndPut(t *testing.T) {
 	}
 }
 
+// Test basic operation in `Heap` mode.
 func TestPutsThenGets(t *testing.T) {
 	const numOfMessages = 5
 
 	box := NewMailBox(numOfMessages)
 
-	// Messages are defined as int. Their value is also used as the priority.
+	// Messages are defined as an int. Their contents are also used as their
+	// priority.
 	messages := make([]int, numOfMessages)
 	for i := range messages {
 		messages[i] = rand.Int()
@@ -86,4 +91,96 @@ func TestPutsThenGets(t *testing.T) {
 			t.Fatalf("'%v' != '%v'", messages[i], received)
 		}
 	}
+}
+
+func TestConcurrently(t *testing.T) {
+	const maxNumOfMessages = 50
+	numOfMessages := rand.Intn(maxNumOfMessages) + 1
+	t.Logf("Num Of Messages: %d", numOfMessages)
+
+	const maxNumOfGetters = 10
+	numOfGetter := rand.Intn(maxNumOfGetters) + 1
+	t.Logf("Num Of Getters: %d", numOfGetter)
+
+	const maxNumOfPutters = 10
+	numOfPutters := rand.Intn(maxNumOfPutters) + 1
+	t.Logf("Num Of Putters: %d", numOfPutters)
+
+	box := NewMailBox(numOfMessages)
+
+	// Messages are defined as an int. Their contents are also used as their
+	// priority.
+	messages := make([]int, numOfMessages)
+	for i := range messages {
+		messages[i] = rand.Int()
+	}
+
+	// Waits for all getters to return.
+	wg := sync.WaitGroup{}
+	wg.Add(numOfGetter)
+
+	// Tells getters to return.
+	ctx, stopGetting := context.WithCancel(context.Background())
+
+	// Used by getters to forward messages to main go-routine.
+	receivedMsgCh := make(chan int, numOfMessages)
+
+	// Start go-routines that get messages.
+	for i := 0; i < numOfGetter; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				message := box.Get(ctx)
+				if message == nil {
+					return
+				}
+				t.Logf("got: '%v'", message)
+				receivedMsgCh <- message.(int)
+			}
+		}()
+	}
+
+	// Used by putters to request another message to put.
+	requestMsgCh := make(chan chan int)
+
+	// Start go-routine to provide the putters with messages.
+	go func() {
+		for i := range messages {
+			respCh := <-requestMsgCh
+			respCh <- messages[i]
+		}
+	}()
+
+	// Setup go-routines that put messages.
+	for i := 0; i < numOfPutters; i++ {
+		go func() {
+			for {
+				ch := make(chan int)
+				requestMsgCh <- ch
+				message := <-ch
+				box.Put(message, message)
+				t.Logf("put: '%v'", message)
+			}
+		}()
+	}
+
+	// Wait for each message to be received once.
+	for range messages {
+		received := <-receivedMsgCh
+
+		found := false
+		for i := range messages {
+			if messages[i] == received {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Fatalf("invalid receive: '%v'", received)
+		}
+	}
+
+	// Shut down go-routines.
+	stopGetting()
+	wg.Wait()
 }
